2a704077b7c2a515fd00c2ef5a2e41e1f57cf200
Quentin731 <quentin.marchandise17@gmail.com>
2021-04-28 10:01:58 +0200

    gestion du filtre

--- src/fr/eni/encheres/ihm/HomePageServlet.java
+++ src/fr/eni/encheres/ihm/HomePageServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import fr.eni.encheres.bll.CategorieManager;
 import fr.eni.encheres.bll.EnchereManager;
@@ -16,9 +17,6 @@ import fr.eni.encheres.bo.BusinessException;
 import fr.eni.encheres.bo.Categorie;
 import fr.eni.encheres.bo.Enchere;
 
-/**
- * Servlet implementation class HomePageServlet
- */
 @WebServlet("/")
 public class HomePageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -45,6 +43,7 @@ public class HomePageServlet extends HttpServlet {
 			request.setAttribute("errorMessage", "La liste des enchères n'est pas disponible");
 		}
 
+		// Gestion affichage de toutes les enchères
 		if (request.getParameter("open") != null) {
 			List<Enchere> listeEnchereOpen;
 			try {
@@ -60,8 +59,53 @@ public class HomePageServlet extends HttpServlet {
 		} else {
 			request.setAttribute("listeEnchere", listeEnchere);
 		}
-		// System.out.println(listeEnchere.get(0).toString());
 
+		// Gestion des ventes en cours
+		if (request.getParameter("VenteEnCours") != null) {
+			List<Enchere> listeEnchereMine = null;
+			Integer idUser = null;
+			List<Enchere> listeAll = new ArrayList<Enchere>();
+			try {
+				HttpSession session = request.getSession();				
+				idUser = (Integer) session.getAttribute("idUser");
+				listeAll = enchereManager.afficherToutesLesEncheres();
+				listeEnchereMine = enchereManager.mesEncheresEnCours(listeAll, idUser);	
+				request.setAttribute("listeEnchere", listeEnchereMine);
+				
+				if (listeEnchereMine.isEmpty()) {
+					throw new BusinessException();
+					}		
+			} catch (BusinessException e) {
+				request.setAttribute("ErrorEnchere", "Pas d'encheres");
+			} 
+		} else {
+			request.setAttribute("listeEnchere", listeEnchere);
+		}
+		
+		// Gestion des ventes non débutées
+		if (request.getParameter("VenteNonDebutees") != null) {
+			List<Enchere> listeEnchereNonDebutees = null;
+			Integer idUser = null;
+			List<Enchere> listeAll = new ArrayList<Enchere>();
+			try {
+				HttpSession session = request.getSession();				
+				idUser = (Integer) session.getAttribute("idUser");
+				listeAll = enchereManager.afficherToutesLesEncheres();
+				listeEnchereNonDebutees = enchereManager.mesEncheresNonDebutees(listeAll, idUser);	
+				request.setAttribute("listeEnchere", listeEnchereNonDebutees);
+				
+				if (listeEnchereNonDebutees.isEmpty()) {
+					throw new BusinessException();
+					}		
+			} catch (BusinessException e) {
+			
+				request.setAttribute("ErrorEnchere", "Pas d'encheres");
+
+			} 
+		} else {
+			request.setAttribute("listeEnchere", listeEnchere);
+		}
+		
 		request.setAttribute("listeCategorie", listeCategorie);
 		request.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(request, response);
 	}
