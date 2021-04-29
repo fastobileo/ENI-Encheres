5e04e3f2dd3fecd188afe36a21b90245ad01fb1f
slydar <tdjennane@gmail.com>
2021-04-29 09:35:15 +0200

    cookie se souvenir de oim

--- src/fr/eni/encheres/ihm/ConnectionPageServlet.java
+++ src/fr/eni/encheres/ihm/ConnectionPageServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,6 +29,17 @@ public class ConnectionPageServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		  Cookie[] cookies = request.getCookies();
+	        if (cookies != null) {
+	            for (Cookie cookie : cookies) {
+	                if (cookie.getName().equals("id")) {
+	                	
+	                    request.setAttribute("id", cookie.getValue());
+	                    System.out.println("il a set le cookies dans la jsp");
+	                }
+	            }
+	        }
 		request.getRequestDispatcher("/WEB-INF/jsp/connection.jsp").forward(request, response);
 	}
 
@@ -35,6 +47,16 @@ public class ConnectionPageServlet extends HttpServlet {
 			throws ServletException, IOException {
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
+		
+		System.out.println(request.getParameter("seSouvenirDeMoi"));
+		if (null != request.getParameter("seSouvenirDeMoi")) {
+			
+	        Cookie cookie = new Cookie("id", id);
+	        cookie.setMaxAge(60 * 60 * 24 * 30);
+	        response.addCookie(cookie);
+	        System.out.println("c Good");
+		}
+		
 		try {
 			Utilisateur user = userManager.seConnecter(id, password);
 			request.setAttribute("user", user);
@@ -42,6 +64,9 @@ public class ConnectionPageServlet extends HttpServlet {
 			session.setAttribute("idUser", user.getId());
 			session.setAttribute("user", (Utilisateur) userManager.afficherUtilisateur(user.getId()));
 			response.sendRedirect(request.getContextPath() + "/");
+			
+			
+			
 		} catch (BusinessException e) {
 			request.setAttribute("errorMessage", "impossible de se connecter");
 			request.getRequestDispatcher("/WEB-INF/jsp/connection.jsp").forward(request, response);
